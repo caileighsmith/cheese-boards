4a88e6bffb439ff6413c25f46bd5f06fcd609331
caileighsmith <caihidden@gmail.com>
2022-08-18 11:16:01 +0100

    Allow body requests to add users.

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ const {User} = require('./users')
 const {Board} = require('./board')
 const {Cheese} = require('./cheese')
 const {db} = require('./db')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+User.hasMany(Board)
+Board.belongsTo(User, {through:'newDb'})
+
+app.post('/user/add', async(req, res)=>{
+    let username = req.body.username
+    let email = req.body.email
+
+    if (username && email){
+        const newUser = await User.create({
+            name: username,
+            email : email
+        })
+        await res.send(newUser)
+
+    }
+
+})
 
 
-
-
-
+app.listen(5000)
 
 
 module.exports = {
--- tests/test.js
+++ tests/test.js
@@ -35,10 +35,47 @@ describe('Testing Cheese model', ()=>{
             title: 'bree',
             description: 'creamy, tasy, delicious.'
         })
-
         expect(testCheese.title).toBe('bree')
         expect(testCheese.description).toBe('creamy, tasy, delicious.')
 
     })
+    test('Cheese Model data types should be strings', async()=>{
+        const testCheese = await Cheese.create({
+            title: 'bree',
+            description: 'creamy, tasy, delicious.'
+        })
+        expect(typeof testCheese.title && typeof testCheese.description).toBe('string')
+    })
 })
+
+describe('Testing Board model', ()=>{
+    beforeAll(async()=>{
+        await db.sync({force:true})
+    })
+    test ('Board model should be created', async()=>{
+        const testBoard = await Board.create({
+            type: 'smorgasbord',
+            description: 'meats, cheases and more.',
+            rating: 10
+        })
+        
+        expect(testBoard.type).toBe('smorgasbord')
+        expect(testBoard.description).toBe('meats, cheases and more.')
+        expect(testBoard.rating).toBe(10)
+    })
+
+    test ('Data types should be correct for Board model', async()=>{
+        const testBoard = await Board.create({
+            type: 'smorgasbord',
+            description: 'meats, cheases and more.',
+            rating: 10
+        })
+        expect (typeof testBoard.type).toBe('string')
+        expect (typeof testBoard.description).toBe('string')
+        expect (typeof testBoard.rating).toBe('number')
+
+
+    })
+})
+
 
